ce255cb8526b076c31699c7f16a4d18b248b2f31
Bhavik Patel <bhavik.patel@gmail.com>
2022-09-10 17:42:26 +0200

    Use __attribute__((__packed__)) to make struct small

--- problems_and_quiz/make_me_small.c
+++ problems_and_quiz/make_me_small.c
@@ -1,36 +1,42 @@
 #include <stdio.h>
- 
-typedef struct structure_1 {
+
+typedef struct __attribute__((__packed__)) structure_1
+{
   short int b;
   char a;
 } structure_1;
- 
-typedef struct structure_2 {
+
+typedef struct __attribute__((__packed__)) structure_2
+{
   int c;
   short int a;
   char b;
 } structure_2;
- 
-typedef struct structure_3 {
+
+typedef struct __attribute__((__packed__)) structure_3
+{
   double b;
   int c;
   char a;
 } structure_3;
- 
-typedef struct structure_4 {
+
+typedef struct __attribute__((__packed__)) structure_4
+{
   double a;
   int b;
   char c;
 } structure_4;
- 
-typedef struct structure_5 {
+
+typedef struct __attribute__((__packed__)) structure_5
+{
   structure_3 a;
   structure_4 c;
   structure_2 d;
   structure_1 b;
 } structure_5;
 
-int main() {
+int main()
+{
   printf("Make me small\n");
   int i = 0;
   size_t tot_structs_size = sizeof(structure_1) + sizeof(structure_2) + sizeof(structure_3) + sizeof(structure_4) + sizeof(structure_5);
